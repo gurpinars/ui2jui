741ea098c56692689ed645db8f3549f02d64571a
Khairul Imam <ki65559@gmail.com>
2017-01-17 17:54:59 +0800

    add convert directory and recursively feature

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 ## ui2jui
-Small tool to convert qt designer ui files to jui(Java user interface definition file) which is required for 
+Small tool to convert qt designer ui files to jui(Java user interface definition file) which is required for
 JUIC(Java User Interface Compiler) to generate java source files.
 ## Usage
-``` 
-python ui2jui somedesignfile.ui 
+**convert one specific ui file**
+```
+python ui2jui.py -f somedesignfile.ui
+```
+
+**convert all ui files from current directory**
+```
+python ui2jui.py -d
+```
+
+**convert all ui files recursively from current directory**
+```
+python ui2jui.py -r
+```
+
+****
+***NOTE: if you want to remove original file after converted just add -rm flag***
+
+####Tips
+add ui2jui.py to your local binary directory just so you can call the ui2jui from any directory you want
+
+```
+sudo ln -s abs_path_to_ui2jui.py /usr/local/bin/ui2jui
+sudo chmod +x /usr/local/bin/ui2jui
 ```
--- ui2jui.py
+++ ui2jui.py
@@ -1,30 +1,68 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
 Small tool to convert qt designer ui files to .jui format which is required
 for qt to use with java language.
-
 """
 
 import argparse
+import os
 
 
 def convert(ui_file):
-    fname = ui_file.split('.')[0] + '.jui'
-        
+    fname = "%s.jui" % os.curdir.join(ui_file.split('.')[:-1])
+
     with open(ui_file, "r") as f:
-        data = f.read()
-        splitting_data = data.split('<?xml version="1.0" encoding="UTF-8"?>')[1]
-        jdata = splitting_data[:18] + ' ' + 'language="jambi"' + splitting_data[18:]    
+        lines = f.readlines()
+        del(lines[0:1])
+        lines[0] = '<ui version="4.0" language="jambi">\n'
 
     with open(fname,'w') as fout:
-        fout.write(jdata.lstrip())      
+        for line in lines:
+            fout.write(line)
 
 
 def main():
     parser = argparse.ArgumentParser(description="converter to .ui files to .jui")
-    parser.add_argument('ui_file', help='ui file to be converted')
+    parser.add_argument('-f', '--file', help='ui file to be converted')
+    parser.add_argument('-d', '--directory', help='convert all files of curdir', action="store_true")
+    parser.add_argument('-r', '--recursive', help='convert all .ui files of curdir', action="store_true")
+    parser.add_argument('-rm', '--remove', help='remove file after converted', action='store_true')
     args = parser.parse_args()
-    convert(args.ui_file)
+    if args.recursive:
+        for file in get_ui_files_from_dir_recursively():
+            convert(file)
+        if args.remove:
+            for file in get_ui_files_from_dir_recursively():
+                del_file(file)
+        print "converted"
+
+    if args.directory:
+        for file in get_ui_files_from_dir():
+            convert(file)
+        if args.remove:
+            for file in get_ui_files_from_dir():
+                del_file(file)
+        print "converted"
+
+    if not (args.directory or args.recursive):
+        convert(args.file)
+        if args.remove:
+            del_file(args.file)
+        print "converted"
+
+def is_file_ends_with_ui(filename):
+    return filename.split('.')[-1] == "ui"
+
+def get_ui_files_from_dir():
+    return [file for file in os.listdir('.') if is_file_ends_with_ui(file)]
+
+def del_file(file):
+    os.remove(file)
+
+
+def get_ui_files_from_dir_recursively():
+    return [os.path.join(dp, f) for dp, dn, filenames in os.walk('.') for f in filenames if is_file_ends_with_ui(f)]
 
 if __name__ == '__main__':
-    main()    
+    main()
